e9c8bc78ee45a2dd9c4268832a83309888c8a460
Minh Vuong <minhvuong@live.com>
2020-04-07 15:06:26 +1000

    starter

---
+++ app.js
@@ -0,0 +1,25 @@
+// Build Board
+
+// Play Game
+
+// Create Players
+
+// Keep track of player's turn
+
+// When you win
+
+/* 
+Create an HTML page that display's a Tic Tac Toe grid
+Ask for player one's name (use prompts or HTML Form)
+Ask for player two's name (use prompts or HTML Form)
+Display a message on the page that says "Joe's Turn" (or whatever player one's name is)
+Let "Joe" take his turn by clicking on a square in the grid
+Mark it with an X (or O)
+Display a message on the page that says "Sue's Turn" (or whatever player two's name is)
+Let "Sue" take her turn by clicking on a square in the grid
+Mark it with an O (or X)
+Continue alternating until there is a winner or a tie
+Display a message declaring the winner (or tie)
+Allow the user to Play Again (without re-entering their names) if they'd like
+Users should not be able to click in a square that has already been marked
+*/
