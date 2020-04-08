5333e759e78ebd1471bb1a7c0ae620c0c0021405
Minh Vuong <minhvuong@live.com>
2020-04-08 17:52:38 +1000

    Added game logic

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var resetBtn = document.querySelector('.reset');
 var instructionsBtn = document.querySelector('.instructions');
 var modal = document.querySelector('.modal');
 var closeModalBtn = document.querySelector('.close');
-var playersTurnDisplay = document.querySelector('.players-turn');
 var nextPlayerDisplay = document.querySelector('.next-player');
 var cellDivs = document.querySelectorAll('.game-cell');
 
 var gameRunning = true;
-var xIsNext = true;
+var oIsNext = true;
 var winner = null;
 var xSymbol = '✖';
 var oSymbol = '✪';
 
+// Functions
 var convertLetter = (letter) => letter === 'x' ? xSymbol : oSymbol; 
 
 var checkForWinner = function(letter) {
@@ -20,7 +20,6 @@ var checkForWinner = function(letter) {
     winner = letter;
     nextPlayerDisplay.classList.add('next-player');
     if (winner === 'x') {
-        nextPlayerDisplay.classList.add('x-winner');
         nextPlayerDisplay.textContent = `${convertLetter(letter)} HAS WON!`;
         nextPlayerDisplay.style.color = 'rgb(255, 196, 40)';
     } else {
@@ -31,71 +30,98 @@ var checkForWinner = function(letter) {
 
 var checkGameStatus = function() {
     // Check whether the cell is a 'x' or 'o'
-    var topLeft = cellDivs[0].classList[2]; 
-    var topMiddle = cellDivs[1].classList[2];
-    var topRight = cellDivs[2].classList[2];
-    var middleLeft = cellDivs[3].classList[2];
-    var middleMiddle = cellDivs[4].classList[2];
-    var middleRight = cellDivs[5].classList[2];
-    var bottomLeft = cellDivs[6].classList[2];
-    var bottomMiddle = cellDivs[7].classList[2];
-    var bottomRight = cellDivs[8].classList[2];
-    console.log(topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight);
+    var topLeft = cellDivs[0].classList[1]; 
+    var topMiddle = cellDivs[1].classList[1];
+    var topRight = cellDivs[2].classList[1];
+    var middleLeft = cellDivs[3].classList[1];
+    var middleMiddle = cellDivs[4].classList[1];
+    var middleRight = cellDivs[5].classList[1];
+    var bottomLeft = cellDivs[6].classList[1];
+    var bottomMiddle = cellDivs[7].classList[1];
+    var bottomRight = cellDivs[8].classList[1];
 
     // Check for winner - winning possibilities
     if (topLeft && topLeft === topMiddle && topLeft === topRight) {
         checkForWinner(topLeft);
+        cellDivs[0].classList.add('won');
+        cellDivs[1].classList.add('won');
+        cellDivs[2].classList.add('won');
     } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
         checkForWinner(middleLeft);
+        cellDivs[3].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[5].classList.add('won');
     } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
         checkForWinner(bottomLeft);
+        cellDivs[6].classList.add('won');
+        cellDivs[7].classList.add('won');
+        cellDivs[8].classList.add('won');
     } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
         checkForWinner(topLeft);
+        cellDivs[0].classList.add('won');
+        cellDivs[3].classList.add('won');
+        cellDivs[6].classList.add('won');
     } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
         checkForWinner(topMiddle);
+        cellDivs[1].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[7].classList.add('won');
     } else if (topRight && topRight === middleRight && topRight === bottomRight) {
         checkForWinner(topRight);
+        cellDivs[2].classList.add('won');
+        cellDivs[5].classList.add('won');
+        cellDivs[8].classList.add('won');
     } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
         checkForWinner(topLeft);
+        cellDivs[0].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[8].classList.add('won');
     } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
         checkForWinner(topRight);
+        cellDivs[2].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[6].classList.add('won');
     }
 }
 
 // Event Handlers
 var handleCellClick = function(event) {
     if (gameRunning) {
-        var classList = event.target.classList;
-        var location = classList[1];
-        
-        if (classList[2] === 'o' || classList[2] === 'x') {
-            return;  // If the cell has been clicked, exit func so cell can't be clicked twice
-
+        // If the cell already has an 'x' or 'o' exit function
+        var targetClassList = event.target.classList;
+        if (targetClassList[1] === 'o' || targetClassList[1] === 'x') { 
+            return;  
         }
-    
-        // If 'x' is next then change to 'o' then switch turns (vice versa)
-        if (xIsNext) {
-            nextPlayerDisplay.innerHTML = '<p>PLAYER <span class="players-turn">✖</span> IS NEXT </p>';
+        // If 'o' is next then next target cell will be 'o' then switch turns
+        if (oIsNext) {
+            targetClassList.add('o');
+            nextPlayerDisplay.innerHTML = '<p>PLAYER <span style="color: rgb(255, 198, 41);" class="players-turn">✖</span> IS NEXT </p>';
             nextPlayerDisplay.style.color = 'rgb(34, 34, 34)';
-            classList.add('o');
-            xIsNext = false;
+            oIsNext = !oIsNext;
             checkGameStatus();
         } else {
+            targetClassList.add('x');
+            oIsNext = !oIsNext;
             nextPlayerDisplay.innerHTML = '<p>PLAYER <span class="players-turn">✪</span> IS NEXT </p>';
             nextPlayerDisplay.style.color = 'rgb(34, 34, 34)';
-            classList.add('x');
-            xIsNext = true;
             checkGameStatus();
+        }
+        // If all cells have been clicked and there is no winner then DRAW
+        if (document.querySelectorAll('.o').length + document.querySelectorAll('.x').length === cellDivs.length && winner === null) {
+            nextPlayerDisplay.textContent = 'DRAW';
         }
     }
 }
 
 var handleReset = function(event) {
     gameRunning = true;
-    cellDivs.forEach(elem => elem.classList.remove('x','o'));
+    winner = null;
+    oIsNext = true;
+    cellDivs.forEach(elem => elem.classList.remove('x','o', 'won'));
     nextPlayerDisplay.innerHTML = '<p>PLAYER <span class="players-turn">✪</span> IS NEXT </p>';
     nextPlayerDisplay.style.color = 'rgb(34, 34, 34)';
 }
+
 // Event listeners
 resetBtn.addEventListener('click', handleReset);
 instructionsBtn.addEventListener('click', function() { modal.style.display = 'block'; });
@@ -104,41 +130,3 @@ cellDivs.forEach(elem => elem.addEventListener('click', handleCellClick));
 window.onclick = function(event) {
     if (event.target == modal) { modal.style.display = "none"; }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-/* 
-Create an HTML page that display's a Tic Tac Toe grid
-Ask for player one's name (use prompts or HTML Form)
-Ask for player two's name (use prompts or HTML Form)
-Display a message on the page that says "Joe's Turn" (or whatever player one's name is)
-Let "Joe" take his turn by clicking on a square in the grid
-Mark it with an X (or O)
-Display a message on the page that says "Sue's Turn" (or whatever player two's name is)
-Let "Sue" take her turn by clicking on a square in the grid
-Mark it with an O (or X)
-Continue alternating until there is a winner or a tie
-Display a message declaring the winner (or tie)
-Allow the user to Play Again (without re-entering their names) if they'd like
-Users should not be able to click in a square that has already been marked
-*/
